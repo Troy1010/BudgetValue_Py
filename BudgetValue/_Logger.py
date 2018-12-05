fa9fcaa20042998e3a15bd6b96d74716547be65c
Unknown <drmcmurderface2@mail.com>
2018-12-05 11:50:35 -0600

    Logger

--- BudgetValue/_Logger.py
+++ BudgetValue/_Logger.py
@@ -1,24 +1,22 @@
-##region Settings
-import os
-bWriteLog = True
-sLogFile = os.path.join(__file__,'..','BVLog.log')
-##endregion
-##region LogInit
 import logging
+import os
+# Settings
+bWriteLog = True
+sLogFile = os.path.join(__file__, '..', 'BVLog.log')
+
 BVLog = logging.getLogger(__name__)
 BVLog.setLevel(logging.DEBUG)
 try:
     os.remove(sLogFile)
-except (PermissionError,FileNotFoundError):
+except (PermissionError, FileNotFoundError):
     pass
 if bWriteLog:
     bLogFileIsOpen = False
     try:
-        os.rename(sLogFile,sLogFile)
+        os.rename(sLogFile, sLogFile)
     except PermissionError:
         bLogFileIsOpen = True
     except FileNotFoundError:
         pass
     if not bLogFileIsOpen:
         BVLog.addHandler(logging.FileHandler(sLogFile))
-##endregion
