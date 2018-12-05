ea9ff105c793bbd7ee8b807f05604e39bc063b7d
Unknown <drmcmurderface2@mail.com>
2018-12-05 04:01:52 -0600

    pages

--- BudgetValue/Main.py
+++ BudgetValue/Main.py
@@ -1,7 +1,10 @@
 import tkinter as TK
 from tkinter import ttk as FancyTK
-
+import matplotlib
+# Globals
 LARGE_FONT = ("Verdana", 12)
+
+matplotlib.use("TkAgg")
 
 
 class BudgetValueApp(TK.Tk):
@@ -19,59 +22,101 @@ class BudgetValueApp(TK.Tk):
 
         self.frames = {}
 
-        for F in (StartPage, PageOne, PageTwo):
+        for F in (SpendingHistory, PaycheckPlan, NetWorth, Spendables, Reports, TestFrame):
             frame = F(container, self)
             self.frames[F] = frame
             # NorthSouthEastWest alignment and stretch
             frame.grid(row=0, column=0, sticky="nsew")
 
-        self.show_frame(StartPage)
+        self.show_frame(SpendingHistory)
 
     def show_frame(self, controller):
         frame = self.frames[controller]
         frame.tkraise()
 
 
-class StartPage(TK.Frame):
+class TestFrame(TK.Frame):
     def __init__(self, parent, controller):
         TK.Frame.__init__(self, parent)
-        vLabel = FancyTK.Label(self, text="Start Page", font=LARGE_FONT)
+        vLabel = FancyTK.Label(self, text="Test Frame", font=LARGE_FONT)
         vLabel.pack(pady=10, padx=10)
 
-        vButton1 = FancyTK.Button(self, text="Page 1",
-                                  command=lambda: controller.show_frame(PageOne))
+        #menubar = TK.Menu(controller)
+        # self.config(menu=menubar)
+        vButton1 = FancyTK.Button(self, text="Spending History",
+                                  command=lambda: controller.show_frame(SpendingHistory))
         vButton1.pack()
-        vButton2 = FancyTK.Button(self, text="Page 2",
-                                  command=lambda: controller.show_frame(PageTwo))
-        vButton2.pack()
 
 
-class PageOne(TK.Frame):
+class SpendingHistory(TK.Frame):
     def __init__(self, parent, controller):
         TK.Frame.__init__(self, parent)
-        vLabel = FancyTK.Label(self, text="Page 1", font=LARGE_FONT)
+        vLabel = FancyTK.Label(self, text="Spending History", font=LARGE_FONT)
         vLabel.pack(pady=10, padx=10)
 
-        vButton1 = FancyTK.Button(self, text="Home",
-                                  command=lambda: controller.show_frame(StartPage))
+        vButton1 = FancyTK.Button(self, text="Paycheck Plan",
+                                  command=lambda: controller.show_frame(PaycheckPlan))
         vButton1.pack()
-        vButton2 = FancyTK.Button(self, text="Page 2",
-                                  command=lambda: controller.show_frame(PageTwo))
-        vButton2.pack()
+
+        vButton1 = FancyTK.Button(self, text="NetWorth",
+                                  command=lambda: controller.show_frame(NetWorth))
+        vButton1.pack()
+
+        vButton1 = FancyTK.Button(self, text="Spendables",
+                                  command=lambda: controller.show_frame(Spendables))
+        vButton1.pack()
+
+        vButton1 = FancyTK.Button(self, text="Reports",
+                                  command=lambda: controller.show_frame(Reports))
+        vButton1.pack()
+
+        vButton1 = FancyTK.Button(self, text="TestFrame",
+                                  command=lambda: controller.show_frame(TestFrame))
+        vButton1.pack()
 
 
-class PageTwo(TK.Frame):
+class PaycheckPlan(TK.Frame):
     def __init__(self, parent, controller):
         TK.Frame.__init__(self, parent)
-        vLabel = FancyTK.Label(self, text="Page 2", font=LARGE_FONT)
+        vLabel = FancyTK.Label(self, text="Paycheck Plan", font=LARGE_FONT)
         vLabel.pack(pady=10, padx=10)
 
-        vButton1 = FancyTK.Button(self, text="Home",
-                                  command=lambda: controller.show_frame(StartPage))
+        vButton1 = FancyTK.Button(self, text="Spending History",
+                                  command=lambda: controller.show_frame(SpendingHistory))
         vButton1.pack()
-        vButton2 = FancyTK.Button(self, text="Page 1",
-                                  command=lambda: controller.show_frame(PageOne))
-        vButton2.pack()
+
+
+class NetWorth(TK.Frame):
+    def __init__(self, parent, controller):
+        TK.Frame.__init__(self, parent)
+        vLabel = FancyTK.Label(self, text="Net Worth", font=LARGE_FONT)
+        vLabel.pack(pady=10, padx=10)
+
+        vButton1 = FancyTK.Button(self, text="Spending History",
+                                  command=lambda: controller.show_frame(SpendingHistory))
+        vButton1.pack()
+
+
+class Spendables(TK.Frame):
+    def __init__(self, parent, controller):
+        TK.Frame.__init__(self, parent)
+        vLabel = FancyTK.Label(self, text="Spendables", font=LARGE_FONT)
+        vLabel.pack(pady=10, padx=10)
+
+        vButton1 = FancyTK.Button(self, text="Spending History",
+                                  command=lambda: controller.show_frame(SpendingHistory))
+        vButton1.pack()
+
+
+class Reports(TK.Frame):
+    def __init__(self, parent, controller):
+        TK.Frame.__init__(self, parent)
+        vLabel = FancyTK.Label(self, text="Reports", font=LARGE_FONT)
+        vLabel.pack(pady=10, padx=10)
+
+        vButton1 = FancyTK.Button(self, text="Spending History",
+                                  command=lambda: controller.show_frame(SpendingHistory))
+        vButton1.pack()
 
 
 app = BudgetValueApp()
