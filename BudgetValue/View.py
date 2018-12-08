d270e85d428b8f4051f5cf8ac7602408b103dfe0
Troy1010 <drmcmurderface2@mail.com>
2018-12-08 04:54:27 -0600

    View-renaming,more Refreshes,popup on click Catagory

--- BudgetValue/View.py
+++ BudgetValue/View.py
@@ -6,7 +6,8 @@ import TM_CommonPy as TM  # noqa
 import BudgetValue as BV
 import itertools
 # Globals
-FONT_LARGE = ("Verdana", 12)
+FONT_LARGE = ("Verdana", 14)
+FONT_MEDIUM = ("Verdana", 12)
 FONT_TEXT = ("Calibri", 10)
 FONT_TEXT_BOLD = ("Calibri", 10, "bold")
 
@@ -60,10 +61,10 @@ class TabBar(tk.Frame):
     def __init__(self, parent, vModel, cTabPages):
         tk.Frame.__init__(self, parent)
         self.cTabButtons = {}
-        for i, vFrame in enumerate(cTabPages):
-            vButton = tk.Button(self, text=vFrame.__name__, width=15,
-                                command=lambda vFrame=vFrame: self.ShowTab(vFrame))
-            self.cTabButtons[vFrame] = vButton
+        for i, page in enumerate(cTabPages):
+            vButton = tk.Button(self, text=page.__name__, width=15, font=FONT_MEDIUM,
+                                command=lambda page=page: self.ShowTab(page))
+            self.cTabButtons[page] = vButton
             vButton.grid(row=0, column=i)
 
     def HighlightButton(self, vButtonToHighlight):
@@ -81,33 +82,40 @@ class SpendingHistory(tk.Frame):
         tk.Frame.__init__(self, parent)
         self.vModel = vModel
         # ButtonBar
-        vButtonFrame = tk.Frame(self)
-        vButtonFrame.pack(side=tk.TOP, anchor='w')
+        self.vButtonFrame = tk.Frame(self)
+        self.vButtonFrame.pack(side=tk.TOP, anchor='w')
         # TableFrame
-        vTableFrame = tk.Frame(self, background='lightgrey')
-        vTableFrame.pack(side=tk.TOP, expand=True, fill="both")
-        vTableFrame.grid_rowconfigure(1, weight=1)
-        vTableFrame.grid_columnconfigure(0, weight=1)
-        vTableFrame.cColWidths = self.GetColWidths(vModel)
+        self.vTableFrame = tk.Frame(self, background='lightgrey')
+        self.vTableFrame.pack(side=tk.TOP, expand=True, fill="both")
+        self.vTableFrame.grid_rowconfigure(1, weight=1)
+        self.vTableFrame.grid_columnconfigure(0, weight=1)
+        self.vTableFrame.parent = self
         #  Header
-        vHeader = self.Header(vTableFrame, vModel)
-        vHeader.grid(row=0, column=0, sticky="NSEW")
+        self.vHeader = self.Header(self.vTableFrame, vModel)
+        self.vHeader.grid(row=0, column=0, sticky="NSEW")
         #  Table
-        self.vTable = self.Table(vTableFrame, vModel)
+        self.vTable = self.Table(self.vTableFrame, vModel)
         self.vTable.grid(row=1, column=0, sticky="NSEW")
         #  Scrollbars
-        vScrollbar_Y = tk.Scrollbar(vTableFrame)
+        vScrollbar_Y = tk.Scrollbar(self.vTableFrame)
         vScrollbar_Y.grid(row=0, rowspan=2, column=1, sticky="ns")
         self.vTable.config(yscrollcommand=vScrollbar_Y.set)
         vScrollbar_Y.config(command=self.vTable.yview)
-        vScrollbar_X = tk.Scrollbar(vTableFrame, orient=tk.HORIZONTAL)
+        vScrollbar_X = tk.Scrollbar(self.vTableFrame, orient=tk.HORIZONTAL)
         vScrollbar_X.grid(row=2, column=0, sticky="ew")
         self.vTable.config(xscrollcommand=vScrollbar_X.set)
         vScrollbar_X.config(command=self.vTable.xview)
         # ButtonBar - continued
-        vButton_ImportHistory = ttk.Button(vButtonFrame, text="Import Spending History",
+        vButton_ImportHistory = ttk.Button(self.vButtonFrame, text="Import Spending History",
                                            command=lambda self=self: self.ImportHistory())
         vButton_ImportHistory.pack(side=tk.LEFT, anchor='w')
+        #
+        self.Refresh()
+
+    def Refresh(self):
+        self.vTableFrame.cColWidths = self.GetColWidths(self.vModel)
+        self.vHeader.Refresh()
+        self.vTable.Refresh()
 
     def ImportHistory(self):
         # Prompt which file
@@ -130,9 +138,19 @@ class SpendingHistory(tk.Frame):
     class Header(tk.Frame):
         def __init__(self, parent, vModel):
             tk.Frame.__init__(self, parent)
-            for j, vItem in enumerate(vModel.SpendingHistory.GetHeader()):
+            self.vModel = vModel
+            self.parent = parent
+
+        def Refresh(self):
+            # Remove old data
+            for vWidget in BV.GetAllChildren(self):
+                if 'tkinter.Text' in str(type(vWidget)):
+                    vWidget.grid_forget()
+                    vWidget.destroy()
+            # Place new data
+            for j, vItem in enumerate(self.vModel.SpendingHistory.GetHeader()):
                 b = tk.Text(self, font=FONT_TEXT_BOLD,
-                            borderwidth=2, width=parent.cColWidths[j], height=1, relief='ridge', background='SystemButtonFace')
+                            borderwidth=2, width=self.parent.cColWidths[j], height=1, relief='ridge', background='SystemButtonFace')
                 b.insert(1.0, str(vItem))
                 b.grid(row=0, column=j)
                 b.configure(state="disabled")
@@ -145,8 +163,6 @@ class SpendingHistory(tk.Frame):
             # Assign TableWindow to Canvas
             self.vTableWindow = tk.Frame(self)
             self.create_window((0, 0), window=self.vTableWindow, anchor='nw')
-            # Table
-            self.Refresh()
 
         def Refresh(self):
             # Remove old data
@@ -162,12 +178,18 @@ class SpendingHistory(tk.Frame):
                     b.insert(1.0, str(vItem))
                     b.grid(row=i, column=j)
                     b.configure(state="disabled")
+                    b.parent = self
             self.update_idletasks()
             # Make scrollable
-            self.vTableWindow.bind("<Configure>", lambda event,
-                                   canvas=self: self.onFrameConfigure())
-            for vWidget in BV.GetAllChildren(self):
+            self.vTableWindow.bind(
+                "<Configure>", lambda event: self.onFrameConfigure())
+            for vWidget in BV.GetAllChildren(self, bIncludeRoot=True):
                 vWidget.bind("<MouseWheel>", self.onMousewheel)
+            # Popup - Select Catagory
+            for cell in self.vTableWindow.children.values():
+                if cell.grid_info()['column'] == 0:
+                    cell.bind(
+                        '<Button-1>', lambda event, cell=cell: self.MakePopup_SelectCatagory(cell))
 
         def onFrameConfigure(self):
             '''Reset the scroll region to encompass the inner frame'''
@@ -176,11 +198,30 @@ class SpendingHistory(tk.Frame):
         def onMousewheel(self, event):
             self.yview_scroll(int(-1 * (event.delta / 120)), "units")
 
+        def MakePopup_SelectCatagory(self, cell):
+            vPopup = SpendingHistory.Popup_SelectCatagory(
+                cell.parent)
+            vPopup.place(x=cell.winfo_x() +
+                         cell.winfo_width(), y=cell.winfo_y())
+            vPopup.tkraise()
+
+    class Popup_SelectCatagory(tk.Frame):
+        previous_popup = None
+
+        def __init__(self, parent):
+            if self.__class__.previous_popup is not None:
+                self.__class__.previous_popup.destroy()
+            tk.Frame.__init__(self, parent)
+            self.__class__.previous_popup = self
+
+            b = tk.Button(self, text="Popip")
+            b.pack()
+
 
 class PaycheckPlan(tk.Frame):
     def __init__(self, parent, vModel):
         tk.Frame.__init__(self, parent)
-        vLabel = ttk.Label(self, text="Paycheck Plan", font=FONT_LARGE)
+        vLabel = ttk.Label(self, text="Paycheck Plan", font=FONT_MEDIUM)
         vLabel.pack(pady=10, padx=10)
 
         vButton1 = ttk.Button(self, text="Spending History",)
@@ -190,7 +231,7 @@ class PaycheckPlan(tk.Frame):
 class NetWorth(tk.Frame):
     def __init__(self, parent, vModel):
         tk.Frame.__init__(self, parent)
-        vLabel = ttk.Label(self, text="Net Worth", font=FONT_LARGE)
+        vLabel = ttk.Label(self, text="Net Worth", font=FONT_MEDIUM)
         vLabel.pack(pady=10, padx=10)
 
         vButton1 = ttk.Button(self, text="Spending History",)
@@ -200,7 +241,7 @@ class NetWorth(tk.Frame):
 class Spendables(tk.Frame):
     def __init__(self, parent, vModel):
         tk.Frame.__init__(self, parent)
-        vLabel = ttk.Label(self, text="Spendables", font=FONT_LARGE)
+        vLabel = ttk.Label(self, text="Spendables", font=FONT_MEDIUM)
         vLabel.pack(pady=10, padx=10)
 
         vButton1 = ttk.Button(self, text="Spending History",)
@@ -210,7 +251,7 @@ class Spendables(tk.Frame):
 class Reports(tk.Frame):
     def __init__(self, parent, vModel):
         tk.Frame.__init__(self, parent)
-        vLabel = ttk.Label(self, text="Reports", font=FONT_LARGE)
+        vLabel = ttk.Label(self, text="Reports", font=FONT_MEDIUM)
         vLabel.pack(pady=10, padx=10)
 
         vButton1 = ttk.Button(self, text="Spending History",)
