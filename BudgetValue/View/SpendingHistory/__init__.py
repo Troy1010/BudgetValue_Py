945464db1682723d52b5dfe35f870a1c19d54cbc
Troy1010 <drmcmurderface2@mail.com>
2018-12-09 08:16:36 -0600

    minor

--- BudgetValue/View/SpendingHistory/__init__.py
+++ BudgetValue/View/SpendingHistory/__init__.py
@@ -2,3 +2,5 @@ from .Header import Header
 from .SelectCatagoryPopup import SelectCatagoryPopup
 from .Table import Table
 from .SpendingHistory import SpendingHistory
+
+__all__ = ['SpendingHistory', 'Header', 'SelectCatagoryPopup', 'Table']
