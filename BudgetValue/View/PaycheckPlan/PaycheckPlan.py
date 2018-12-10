2c89aae599abd25b0c9c53c11672216909674a78
Troy1010 <drmcmurderface2@mail.com>
2018-12-10 13:42:08 -0600

    View-PaycheckPlan-Entry_FocusIn

--- BudgetValue/View/PaycheckPlan/PaycheckPlan.py
+++ BudgetValue/View/PaycheckPlan/PaycheckPlan.py
@@ -10,8 +10,25 @@ class PaycheckPlan(tk.Frame):
         tk.Frame.__init__(self, parent)
 
         for i, vCategory in enumerate(vModel.Categories.GetTrueCategories()):
-            b = tk.Text(self, font=Fonts.FONT_SMALL, width=15,
-                        borderwidth=2, height=1, relief='ridge', background='SystemButtonFace')
-            b.insert(1.0, str(vCategory.name))
-            b.grid(row=i)
-            b.configure(state="disabled")
+            self.MakeText((i, 0), str(vCategory.name))
+            self.MakeEntry((i, 1))
+
+    def MakeText(self, cRowColumnPair, text):
+        w = tk.Text(self, font=Fonts.FONT_SMALL, width=15,
+                    borderwidth=2, height=1, relief='ridge', background='SystemButtonFace')
+        w.insert(1.0, text)
+        w.grid(row=cRowColumnPair[0], column=cRowColumnPair[1])
+        w.configure(state="disabled")
+
+    def MakeEntry(self, cRowColumnPair):
+        w = tk.Entry(self, font=Fonts.FONT_SMALL, width=15, justify=tk.RIGHT,
+                     borderwidth=2, relief='ridge', background='SystemButtonFace')
+        w.grid(row=cRowColumnPair[0], column=cRowColumnPair[1])
+        w.bind("<FocusIn>", lambda event, w=w: self.Entry_FocusIn(event, w))
+        w.bind("<FocusOut>", lambda event, w=w: self.Entry_FocusOut(event, w))
+
+    def Entry_FocusIn(self, event, w):
+        w.config(justify=tk.LEFT)
+
+    def Entry_FocusOut(self, event, w):
+        w.config(justify=tk.RIGHT)
