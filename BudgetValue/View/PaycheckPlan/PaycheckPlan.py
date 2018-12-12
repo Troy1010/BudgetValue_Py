cae8608ec5156c38961dcdbeca94fd17d66926a6
Troy1010 <drmcmurderface2@mail.com>
2018-12-11 21:18:30 -0600

    minor

--- BudgetValue/View/PaycheckPlan/PaycheckPlan.py
+++ BudgetValue/View/PaycheckPlan/PaycheckPlan.py
@@ -14,11 +14,14 @@ class PaycheckPlan(tk.Frame):
         # ButtonBar
         self.vButtonBar = tk.Frame(self)
         self.vButtonBar.pack(side=tk.TOP, anchor='w')
-        vButton_Save = ttk.Button(self.vButtonBar, text="Save", command=lambda self=self: self.vModel.PaycheckPlan.Save())
+        vButton_Save = ttk.Button(self.vButtonBar, text="Save",
+                                  command=lambda self=self: self.vModel.PaycheckPlan.Save())
         vButton_Save.pack(side=tk.LEFT, anchor='w')
-        vButton_Load = ttk.Button(self.vButtonBar, text="Load", command=lambda self=self: self.Load())
+        vButton_Load = ttk.Button(self.vButtonBar, text="Load",
+                                  command=lambda self=self: self.Load())
         vButton_Load.pack(side=tk.LEFT, anchor='w')
-        vButton_Print = ttk.Button(self.vButtonBar, text="Print", command=lambda self=self: print(self.vModel.PaycheckPlan.Narrate()))
+        vButton_Print = ttk.Button(self.vButtonBar, text="Print",
+                                   command=lambda self=self: print(self.vModel.PaycheckPlan.Narrate()))
         vButton_Print.pack(side=tk.LEFT, anchor='w')
         # Table
         self.vTable = Table(self, vModel)
