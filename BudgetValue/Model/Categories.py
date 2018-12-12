a38c1a94db51f11dd44cb232d65636887db6f113
Troy1010 <drmcmurderface2@mail.com>
2018-12-12 10:02:32 -0600

    Model-Categories-type

--- BudgetValue/Model/Categories.py
+++ BudgetValue/Model/Categories.py
@@ -1,31 +1,49 @@
 from BudgetValue._Logger import BVLog  # noqa
 import TM_CommonPy as TM  # noqa
+from enum import Enum
+import enum
+
+
+class AutoName(Enum):
+    def _generate_next_value_(name, start, count, last_values):
+        return name
+
+
+class CategoryType(AutoName):
+    extra = enum.auto()
+    income = enum.auto()
+    always = enum.auto()
+    reservoir = enum.auto()
+    once = enum.auto()
+    excess = enum.auto()
 
 
 class Categories(dict):
     def __init__(self, vModel):
         self.vModel = vModel
-        default_catagory_names = [
-            "<Default Category>",
-            "Rent",
-            "Hair",
-            "Commute",
-            "Christmas",
-            "Food",
-            "Food-Vanity",
-            "Emergency",
-            "Improvements",
-            "Activities",
-            "Savings"
+        default_catagories = [
+            Category("<Default Category>", CategoryType.extra),
+            Category("Rent", CategoryType.always),
+            Category("Hair", CategoryType.always),
+            Category("Commute", CategoryType.always),
+            Category("Christmas", CategoryType.always),
+            Category("Food", CategoryType.always),
+            Category("Hair", CategoryType.always),
+            Category("Food-Vanity", CategoryType.always),
+            Category("Emergency", CategoryType.always),
+            Category("Improvements", CategoryType.always),
+            Category("Activities", CategoryType.always),
+            Category("Savings", CategoryType.excess)
         ]
-        for i, name in enumerate(default_catagory_names):
-            self[name] = Category(name)
+        for category in default_catagories:
+            self[category.name] = category
 
     def GetTrueCategories(self):
-        return [category for category in self.values() if "<" not in category.name]
+        return [category for category in self.values() if category.type != CategoryType.extra]
 
 
 class Category():
-    def __init__(self, name):
+    def __init__(self, name, type_):
         self.name = name
+        self.type = type_
         self.bFavorite = False
