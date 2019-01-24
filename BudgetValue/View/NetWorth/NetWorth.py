49fce4627d4adc96ca23c13db25acccf2d387d3e
Unknown <drmcmurderface2@mail.com>
2019-01-24 16:26:10 -0600

    View-NetWorth-TriggerChange debug button

--- BudgetValue/View/NetWorth/NetWorth.py
+++ BudgetValue/View/NetWorth/NetWorth.py
@@ -18,12 +18,20 @@ class NetWorth(tk.Frame):
         vButton_AddRow = ttk.Button(self.vButtonBar, text="AddRow",
                                     command=lambda self=self: self.AddRow())
         vButton_AddRow.pack(side=tk.LEFT, anchor='w')
+        vButton_TriggerNonDistinctChange = ttk.Button(self.vButtonBar, text="TriggerChange",
+                                                      command=lambda self=self: self.TriggerChange())
+        vButton_TriggerNonDistinctChange.pack(side=tk.LEFT, anchor='w')
         # Table
         self.vCanvas = tk.Canvas(self, highlightthickness=0)
         self.vCanvas.pack(side=tk.TOP, fill='x', anchor='nw')
         self.vTable = Table(self.vCanvas, vModel)
         self.vCanvas.create_window((0, 0), window=self.vTable, anchor='nw')
         self.vTable.pack(anchor='nw')
+        self.vTable.Refresh()
+
+    def TriggerChange(self):
+        if len(self.vModel.NetWorth) > 2:
+            self.vModel.NetWorth[0], self.vModel.NetWorth[1] = self.vModel.NetWorth[1], self.vModel.NetWorth[0]
         self.vTable.Refresh()
 
     def AddRow(self):
