374c0122f4caac2f1568d71ebe158a5ecf5e3d55
Unknown <drmcmurderface2@mail.com>
2019-01-25 15:11:28 -0600

    exploring observables

--- BudgetValue/Model/NetWorth.py
+++ BudgetValue/Model/NetWorth.py
@@ -2,6 +2,7 @@ import BudgetValue as BV
 import os
 import pickle
 import rx
+import TM_CommonPy as TM  # noqa
 
 
 class NetWorth(list):
@@ -10,6 +11,106 @@ class NetWorth(list):
         self.vModel = vModel
         self.sSaveFile = os.path.join(self.vModel.sWorkspace, "NetWorth.pickle")
         # Structure total stream
+        # --
+
+        def Scan_Collect2(accumulator, value):
+            print("value:"+str(value))
+            print("Scan_Collect2")
+            if value.bAdd:
+                accumulator.add(value.stream)
+            else:
+                accumulator.remove(value.stream)
+            return accumulator
+
+        def Scan_CalcTotal(accumulator, value):  # Gets cStreams
+            print("Scan_CalcTotal. difference:"+str(value)+" total:"+str(accumulator + value))
+            return accumulator + value
+
+        def pseudo_lambda3(cStreams):
+            print("switch_map. cStreams:"+str(cStreams))
+            if not cStreams:  # all amount streams are empty
+                return rx.Observable.of(0)
+            else:
+                return rx.Observable.combine_latest(list(cStreams), lambda *args: args)
+
+        def ScanAmountToDifference(accumulator, value):
+            print("ScanAmountToDifference. old:"+str(accumulator)+" new:"+str(value)+" diff:"+str(value - accumulator))
+            accumulator = value - accumulator
+            return accumulator
+
+        def ConvertAmountStreamsToDifferenceStreams(cStreams):
+            print("ConvertAmountStreamsToDifferenceStreams. cStreams:"+str(cStreams))
+            cReturning = [x.scan(ScanAmountToDifference, 0) for x in cStreams]
+            print("ConvertAmountStreamsToDifferenceStreams. cReturning:"+str(cReturning))
+            return cReturning
+
+        def MergeStreams(cStreams):
+            if not cStreams:  # all amount streams are empty
+                return rx.Observable.of(0)
+            else:
+                return rx.Observable.merge(cStreams)
+
+        self.new_stream = rx.subjects.Subject()
+        self.total_stream2 = self.new_stream.scan(  # getting AddStreamPair
+            Scan_Collect2,
+            set()
+        ).map(  # getting collection of amount streams
+            ConvertAmountStreamsToDifferenceStreams
+        ).switch_map(  # getting collection of difference streams
+            MergeStreams
+        ).scan(  # getting merged difference stream
+            Scan_CalcTotal,
+            0
+        )
+        self.s1 = rx.subjects.ReplaySubject()
+        self.s1.subscribe(lambda x: print("s1:"+str(x)))
+        self.new_stream.subscribe(lambda x: print("new_stream:"+str(x)))
+        self.total_stream2.subscribe(lambda x: print("total_stream2:"+str(x)))
+
+        class StreamAddPair():
+            def __init__(self, bAdd, stream):
+                self.bAdd = bAdd
+                self.stream = stream
+        self.new_stream.on_next(StreamAddPair(True, self.s1))
+        self.s1.on_next(4)
+        self.s1.on_next(7)
+        self.s2 = rx.subjects.ReplaySubject()
+        self.s2.subscribe(lambda x: print("s2:"+str(x)))
+        self.new_stream.on_next(StreamAddPair(True, self.s2))
+        self.s2.on_next(6)
+        self.s2.on_next(12)
+        self.s2.on_next(10)
+        self.s1.on_next(10)
+        print("FIRST PART DONE")
+        # -----
+
+        def Scan_Collect(accumulator, value):
+            accumulator.append(value)
+            return accumulator
+
+        def pseudo_lambda2(cStreams):
+            if not cStreams:  # all amount streams are empty
+                return rx.Observable.of(0)
+            else:
+                return rx.Observable.combine_latest(cStreams, lambda *args: args)
+
+        self.newStream = rx.subjects.Subject()
+        self.streamOfStreams = self.newStream.scan(
+            Scan_Collect,
+            []
+        ).switch_map(
+            pseudo_lambda2
+        ).map(
+            lambda cValues: ''.join(cValues)
+        ).subscribe(lambda x: print(str(x)))
+        self.s1 = rx.subjects.ReplaySubject()
+        self.newStream.on_next(self.s1)
+        self.s1.on_next('A')
+        self.s1.on_next('B')
+        self.s2 = rx.subjects.ReplaySubject()
+        self.newStream.on_next(self.s2)
+        self.s2.on_next('C')
+        # -----
         self.cStreams_stream = rx.subjects.Subject()
 
         def pseudo_lambda(cStreams):
