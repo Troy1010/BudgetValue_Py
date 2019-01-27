8706340e0670a60b759bed9924b067ca21fdcabe
Unknown <drmcmurderface2@mail.com>
2019-01-27 03:25:45 -0600

    (incomplete) SpendingHistory-inherits Misc.Dict_TotalStream

--- BudgetValue/Model/SplitMoneyHistory.py
+++ BudgetValue/Model/SplitMoneyHistory.py
@@ -1,7 +1,8 @@
 import os
 import pickle
 import BudgetValue as BV
-from decimal import Decimal
+import rx
+from . import Misc
 
 
 class SplitMoneyHistory(list):
@@ -18,12 +19,7 @@ class SplitMoneyHistory(list):
         del self[iColumn][categoryName]
 
     def AddColumn(self):
-        self.append(dict())
-        self._AddBalanceEntry(-1)
-
-    def _AddBalanceEntry(self, iColumn):
-        category = self.vModel.Categories["<Default Category>"]
-        self[-1][category.name] = BalanceEntry(self[-1], category)
+        self.append(SplitMoneyHistoryColumn())
 
     def AddEntry(self, iColumn, category, amount=0):
         self[iColumn][category.name] = SplitMoneyHistoryEntry(category=category, amount=amount)
@@ -51,45 +47,22 @@ class SplitMoneyHistory(list):
                     self.AddEntry(-1, entry["category"], amount=entry["amount"])
 
 
-class BalanceEntry(dict):
-    def __init__(self, parent, category):
-        self.parent = parent
+class SplitMoneyHistoryColumn(Misc.Dict_TotalStream):
+    def __init__(self):
+        super().__init__()
+        self["<Default Category>"] = Misc.BalanceEntry(self)
+
+
+class SplitMoneyHistoryEntry():
+    def __init__(self, category=None, amount=0):
         self.category = category
-
-    @property
-    def amount(self):
-        dBalance = Decimal(0)
-        for item in self.parent.values():
-            if item is not None and "amount" in item:
-                dBalance += 0 if item.amount is None else item.amount
-        return -dBalance
-
-    @property
-    def category(self):
-        return self["category"]
-
-    @category.setter
-    def category(self, value):
-        self["category"] = value
-
-
-class SplitMoneyHistoryEntry(dict):
-    def __init__(self, category=None, amount=None):
-        self.category = category
+        self._amount_stream = rx.subjects.BehaviorSubject(0)
         self.amount = amount
 
     @property
     def amount(self):
-        return self["amount"]
+        return self._amount_stream.value
 
     @amount.setter
     def amount(self, value):
-        self["amount"] = None if not value or value == 0 else BV.MakeValid_Money(value)
-
-    @property
-    def category(self):
-        return self["category"]
-
-    @category.setter
-    def category(self, value):
-        self["category"] = value
+        self._amount_stream.on_next(BV.MakeValid_Money(value))
