3e8df834ae097f8935ede90682ffc09d2a36172c
Troy1010 <drmcmurderface2@mail.com>
2019-02-01 03:02:16 -0600

    View-Misc- BudgetedTable now inherits CategoryTable

--- BudgetValue/View/Misc.py
+++ BudgetValue/View/Misc.py
@@ -6,15 +6,17 @@ import BudgetValue as BV
 from . import WidgetFactories as WF
 
 
-class BudgetedTable(TM.tk.TableFrame):
+class CategoryTable(TM.tk.TableFrame):
+    iFirstDataColumn = 1
+    iFirstDataRow = 1
+
     def __init__(self, parent, vModel):
         assert isinstance(vModel, BV.Model.Model)
         tk.Frame.__init__(self, parent)
         self.vModel = vModel
         self.parent = parent
         self.cDisposables = []
-        self.iFirstDataColumn = 2
-        self.iFirstDataRow = 1
+        self._cRowToCategory = {}
 
     def Refresh(self):
         # remove old
@@ -26,16 +28,75 @@ class BudgetedTable(TM.tk.TableFrame):
                 disposable.dispose()
         self.cDisposables = []
         # add new
-        row = 0
         # Column Header
-        WF.MakeHeader(self, (row, 0), text="Category")
+        WF.MakeHeader(self, (0, 0), text="Category")
+        # if the name of the caller is Refresh, then we are trusting that that will call FinishRefresh when it's done
+        if TM.FnName(1) != "Refresh":
+            self.FinishRefresh()
+
+    def FinishRefresh(self):
+        self.AddRowHeaders()
+        self.AddSeparationLables()
+        self.PopulateRowToCategory()
+
+    def PopulateRowToCategory(self):
+        # Populate _cRowToCategory
+        row = self.iFirstDataRow
+        max_row = self.GetMaxRow()
+        while row <= max_row:
+            category = self.__GetCategoryOfRow_ByRowHeaderText(row)
+            if category is not None:
+                self._cRowToCategory[row] = category
+            row += 1
+
+    def AddSeparationLables(self):
+        prev_type = None
+        row = self.iFirstDataRow
+        max_row = self.GetMaxRow()
+        while row <= max_row:
+            category = self.__GetCategoryOfRow_ByRowHeaderText(row)
+            if category is None:
+                row += 1
+                continue
+            if prev_type != category.type:
+                prev_type = category.type
+                self.InsertRow(row)
+                WF.MakeSeparationLable(self, row, "  " + prev_type.name.capitalize())
+                max_row += 1
+                row += 1
+            row += 1
+
+    def GetCategoryOfRow(self, row):
+        return self.__cRowToCategory[row]
+
+    def __GetCategoryOfRow_ByRowHeaderText(self, row):
+        try:
+            return self.vModel.Categories[self.GetCell(row, 0).text]
+        except AttributeError:  # cell does not have .text
+            return None
+
+    def AddRowHeaders(self):
+        for row, category in enumerate(self.vModel.Categories.Select()):
+            # Row Header
+            if not self.GetCell(row+self.iFirstDataRow, 0) and not self.IsRowEmpty(row+self.iFirstDataRow):
+                WF.MakeEntry_ReadOnly(self, (row+self.iFirstDataRow, 0), text=category.name, justify=tk.LEFT, bBold=True)
+
+
+class BudgetedTable(CategoryTable):
+    iFirstDataColumn = 2
+
+    def Refresh(self):
+        super().Refresh()
+        #
+        row = 0
+        #
         WF.MakeHeader(self, (row, 1), text="Budgeted", background=vSkin.BG_BUDGETED)
         row += 1
         # Data
         for category in self.vModel.Categories.Select():
             # Budgeted
             if category.name in self.vModel.BudgetedSpendables.cCategoryTotalStreams:
-                w = self.MakeEntry_ReadOnly((row, 1), text=self.vModel.BudgetedSpendables.cCategoryTotalStreams[category.name])
+                w = self.__BudgetedCell((row, 1), text=self.vModel.BudgetedSpendables.cCategoryTotalStreams[category.name])
             # Highlight Budgeted
 
             def HighlightBudgeted(budgeted_amount, w):
@@ -53,42 +114,9 @@ class BudgetedTable(TM.tk.TableFrame):
         if TM.FnName(1) != "Refresh":
             self.FinishRefresh()
 
-    def MakeEntry_ReadOnly(self, *args, **kwargs):
+    def __BudgetedCell(self, *args, **kwargs):
         w = WF.MakeEntry_ReadOnly(self, *args, **kwargs)
         # validation
         w.ValidationHandler = BV.MakeValid_Money_ZeroIsNone
         #
         return w
-
-    def FinishRefresh(self):
-        self.AddRowHeaders()
-        self.AddSeparationLables()
-
-    def AddSeparationLables(self):
-        prev_type = None
-        row = self.iFirstDataRow
-        max_row = self.GetMaxRow()
-        while row <= max_row:
-            category = self.GetCategoryOfRow(row)
-            if category is None:
-                row += 1
-                continue
-            if prev_type != category.type:
-                prev_type = category.type
-                self.InsertRow(row)
-                WF.MakeSeparationLable(self, row, "  " + prev_type.name.capitalize())
-                max_row += 1
-                row += 1
-            row += 1
-
-    def GetCategoryOfRow(self, row):
-        try:
-            return self.vModel.Categories[self.GetCell(row, 0).text]
-        except AttributeError:  # cell does not have .text
-            return None
-
-    def AddRowHeaders(self):
-        for row, category in enumerate(self.vModel.Categories.Select()):
-            # Row Header
-            if not self.GetCell(row+self.iFirstDataRow, 0) and not self.IsRowEmpty(row+self.iFirstDataRow):
-                WF.MakeEntry_ReadOnly(self, (row+self.iFirstDataRow, 0), text=category.name, justify=tk.LEFT, bBold=True)
