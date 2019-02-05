85f3cf5af64922af1e66e9e2472acab9ab6dd3cf
Troy1010 <drmcmurderface2@mail.com>
2019-02-04 19:52:25 -0600

    Popup_SelectDate- ValidateDay uses month and year

--- BudgetValue/View/Popup_SelectDate.py
+++ BudgetValue/View/Popup_SelectDate.py
@@ -35,8 +35,34 @@ class Popup_SelectDate(tk.Frame):
         def ValidateMonth(x):
             return TM.WithinRange(int(x), 1, 12)
 
-        def ValidateDay(x):
-            return TM.WithinRange(int(x), 1, 31)
+        def ValidateDay(x, year, month):
+            if month == 1:
+                max_ = 31
+            elif month == 2:
+                max_ = 28
+                if year % 4 == 0:  # leap year
+                    max_ += 1
+            elif month == 3:
+                max_ = 31
+            elif month == 4:
+                max_ = 30
+            elif month == 5:
+                max_ = 31
+            elif month == 6:
+                max_ = 30
+            elif month == 7:
+                max_ = 31
+            elif month == 8:
+                max_ = 31
+            elif month == 9:
+                max_ = 30
+            elif month == 10:
+                max_ = 31
+            elif month == 11:
+                max_ = 30
+            elif month == 12:
+                max_ = 31
+            return TM.WithinRange(int(x), 1, max_)
 
         def ValidateHour(x):
             return TM.WithinRange(int(x), 1, 12)
@@ -48,11 +74,15 @@ class Popup_SelectDate(tk.Frame):
         year = rx.subjects.BehaviorSubject(dt.year)
         month = rx.subjects.BehaviorSubject(dt.month)
         day = rx.subjects.BehaviorSubject(dt.day)
+        rx.Observable.combine_latest(  # days validation rely on year and month
+            year, month,
+            lambda year, month: ValidateDay(day.value, year, month)
+        ).subscribe(day)
         hour = rx.subjects.BehaviorSubject(dt.hour)
         minute = rx.subjects.BehaviorSubject(dt.minute)
         self.MakeEntry((0, 0), year, validation=ValidateYear)
         self.MakeEntry((1, 0), month, validation=ValidateMonth)
-        self.MakeEntry((2, 0), day, validation=ValidateDay)
+        self.MakeEntry((2, 0), day, validation=lambda x, month_stream=month, year_stream=year: ValidateDay(x, year_stream.value, month_stream.value))
         self.MakeEntry((3, 0), hour, validation=ValidateHour)
         self.MakeEntry((4, 0), minute, validation=ValidateMinute)
         self.timestamp = rx.subjects.BehaviorSubject(0)
@@ -65,14 +95,12 @@ class Popup_SelectDate(tk.Frame):
         year.on_next(year.value)
         # Bind Return to accept
 
-        def ReturnIt(event):
-            # print(TM.Narrate(event))
+        def OnReturn(event):
             if isinstance(event.widget, tk.Entry):
                 return
-            print("ReturnIt")
             timestamp_handler(self.timestamp.value)
             self.destroy()
-        self.winfo_toplevel().bind("<Return>", ReturnIt, add='+')
+        self.winfo_toplevel().bind("<Return>", OnReturn, add='+')
         # OK button
         WF.MakeX(self, (5, 0), lambda: (timestamp_handler(self.timestamp.value), self.destroy()))
         # Bind Escape to exit
