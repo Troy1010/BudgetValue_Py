ebc7eaa3aca2180d361acd51608e1d95b463e13f
Troy1010 <drmcmurderface2@mail.com>
2019-07-30 22:13:40 -0500

    Model-Removed legacy sql code

--- BudgetValue/Model/Model.py
+++ BudgetValue/Model/Model.py
@@ -1,7 +1,6 @@
 from BudgetValue._Logger import BVLog  # noqa
 import TM_CommonPy as TM  # noqa
 import BudgetValue as BV
-import sqlite3
 import os
 
 
@@ -9,9 +8,6 @@ class Model():
     def __init__(self):
         self.sWorkspace = os.path.expanduser("~/Documents/BudgetValue/")
         TM.TryMkdir(self.sWorkspace)
-        self.sSpendingHistoryFile = os.path.join(self.sWorkspace, "SpendingsHistory.db")
-        self.connection = sqlite3.connect(self.sSpendingHistoryFile)
-        self.connection.row_factory = sqlite3.Row
         #
         self.Categories = BV.Model.Categories(self)
         self.PaycheckPlan = BV.Model.PaycheckPlan(self)
