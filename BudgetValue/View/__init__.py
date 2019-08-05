e61bbc48057c0ae418df9662f00d4ad2d4c20b8f
Troy1010 <drmcmurderface2@mail.com>
2019-08-05 01:19:02 -0500

    Popup_SelectCategoryType deleted

--- BudgetValue/View/__init__.py
+++ BudgetValue/View/__init__.py
@@ -5,10 +5,9 @@ from .Popup_SelectCategory import Popup_SelectCategory
 from .Popup_SelectDate import Popup_SelectDate
 from .Popup_InputText import Popup_InputText
 from .Popup_AddCategory import Popup_AddCategory
-from .Popup_SelectCategoryType import Popup_SelectCategoryType
 from .Popup_SelectFromList import Popup_SelectFromList
 
 
 __all__ = ['View', 'PaycheckPlan', 'Accounts', 'Popup_SelectCategory',
            'Popup_SelectDate', 'Popup_AddCategory', 'Popup_InputText',
-           'Popup_SelectCategoryType', 'Popup_SelectFromList']
+           'Popup_SelectFromList']
